d4893eade6251b3e5a4134d2ed55b99a44630c99
Agustin Juarez <agustin.juarez8483@gmail.com>
2023-09-09 13:55:01 -0300

    0.3

--- main.ts
+++ main.ts
@@ -4,3 +4,8 @@ const persona1 = new Persona("Juan", "Pérez", new Date(1990, 0, 15), 175, 70, t
 const persona2 = new Persona("Fede", "Lopez", new Date(1983, 3, 25), 190, 78, false);
 
 console.log('esta todo bien');
+persona1.setNombre("NuevoNombre");
+console.log(`Nombre: ${persona1.getNombre()}`);
+console.log(`Apellido: ${persona1.getApellido()}`);
+console.log(`Nombre: ${persona2.getNombre()}`);
+console.log(`Apellido: ${persona2.getApellido()}`);
