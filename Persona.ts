052f320410e0c182669feb3531c40dc828dcbbd3
Agustin Juarez <agustin.juarez8483@gmail.com>
2023-09-09 13:32:01 -0300

    0.2

--- Persona.ts
+++ Persona.ts
@@ -1,3 +1,17 @@
 export class Persona {
+    nombre: string;
+    apellido: string;
+    fechaNacimiento: Date;
+    altura: number;
+    peso: number;
+    estaVivo: boolean;
 
+    constructor(nombre: string, apellido: string, fechaNacimiento: Date, altura: number, peso: number, estaVivo: boolean) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNacimiento = fechaNacimiento;
+        this.altura = altura;
+        this.peso = peso;
+        this.estaVivo = estaVivo;
+    } 
 }
--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import { Persona } from "./persona"; //
 
-const persona1 = new Persona();
-const persona2 = new Persona();
+const persona1 = new Persona("Juan", "Pérez", new Date(1990, 0, 15), 175, 70, true);
+const persona2 = new Persona("Fede", "Lopez", new Date(1983, 3, 25), 190, 78, false);
 
 console.log('esta todo bien');
